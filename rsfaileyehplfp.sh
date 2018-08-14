ea4506e09c232ee5d6361814e7fc3cd1cf3cbada
akihumii <a0126587@u.nus.edu>
2018-08-14 17:37:02 +0800

    changed a bit of cs5, removeanalog and rsfaileyehplfp

--- rsfaileyehplfp.sh
+++ rsfaileyehplfp.sh
@@ -5,7 +5,8 @@ cwd=`pwd`
 for i in $(cat hmmfail.txt nolearndirs.txt | sort | uniq); do
     echo $i
     cd $i
-    rm -r hmmsort decode* learn* eyehplfpHPC* rplhighpass.mat rpllfp.mat transfer*
+    rm -r $(ls | grep -v rplraw.mat)
+#    rm -r hmmsort decode* learn* eyehplfpHPC* rplhighpass.mat rpllfp.mat transfer*
     qsub $GITHUB_MATLAB/Hippocampus/Compiler/hplfp/eyehplfpHPC_submit_file.txt
     cd $cwd
 done
