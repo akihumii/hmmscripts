06d75dbf2c4ad5fec51cea611c8f99f3706a2991
akihumii <a0126587@u.nus.edu>
2018-09-10 21:55:10 +0800

    modified transfersession.sh to transfer more completed files

--- transfersession.sh
+++ transfersession.sh
@@ -10,7 +10,7 @@ targetDir+=$sessionDir
 echo $targetDir
 
 ssh -p 8398 hippocampus@cortex.nus.edu.sg mkdir -p $targetDir
-scp -P 8398 -vr 20180910_Notes.txt CellActivityLog.new.xlsx Electrode\ movement\ notes.xlsx P9_10.edf results_Set1_2018_9_10.mat 18* analog rplparallel.mat rsData.mat rplsplit.mat rsHPC_submit_file.txt.* nolearndirs.txt nodecodedirs.txt hmmfail.txt RawData_T1* hippocampus@cortex.nus.edu.sg:$targetDir &&
-rm -rv * &&
+scp -P 8398 -vr  *.xlsx *.edf *.png 18* analog *.mat rsHPC_submit_file.txt.* *.txt RawData_T1* hippocampus@cortex.nus.edu.sg:$targetDir 
+rm -rv * 
 touch transferredSession.txt
 
