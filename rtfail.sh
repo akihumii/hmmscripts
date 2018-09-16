560df9de58324f9deebc64126da3fe2e4ec400ff
akihumii <a0126587@u.nus.edu>
2018-09-16 22:09:35 +0800

    changed source directory of the matlab file to a more general one

--- rtfail.sh
+++ rtfail.sh
@@ -6,7 +6,7 @@ for i in $(cat notransferreddirs.txt); do
     echo $i
     cd $i
     rm transfer_job0001.pbs.*
-    qsub ~/hmmsort/transfer_job0001.pbs
+    qsub ~/matlab/Hippocampus/transfer_job0001.pbs
     cd $cwd
 done
 
