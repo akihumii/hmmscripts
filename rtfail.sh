cd42146ee2afe286971f531d3145edc2c96304c5
akihumii <a0126587@u.nus.edu>
2018-09-18 15:16:46 +0800

    modified rtfail.sh so that it wont use a tar file to transfer anymore but using scp to transfer all the things in current directories

--- rtfail.sh
+++ rtfail.sh
@@ -5,8 +5,7 @@ cwd=`pwd`
 for i in $(cat notransferreddirs.txt); do
     echo $i
     cd $i
-    rm transfer_job0001.pbs.*
-    qsub ~/matlab/Hippocampus/transfer_job0001.pbs
+    qsub ~/hmmscripts/transfersession.pbs
     cd $cwd
 done
 
