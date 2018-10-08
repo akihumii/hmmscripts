f56370179e487c6ea7a0be276c15401a9c5c607b
akihumii <a0126587@work11.local>
2018-10-08 10:33:47 +0800

    edited checkLsAll.sh function to print out the number of the directories

--- checkLsAll.sh
+++ checkLsAll.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+printLsAll()
+{
 find . -name 'channel???' | while IFS= read i; do
 
 echo $i' '$(ls $i | wc -l)
 
 done 
+}
+
+# Print all the ls
+printLsAll
+
+# Print the number of the directories
+printLsAll | wc -l  
